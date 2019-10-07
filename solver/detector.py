2a706f774986d5c5a1cc8d6c90815f9bbe56183a
Prateeksha Singh <pratu16x7@gmail.com>
2019-10-07 18:37:37 +0530

    use detector to display pattern, edges and stats

--- solver/detector.py
+++ solver/detector.py
@@ -1,6 +1,145 @@
 import cv2
 import numpy as np
 
+CELL_SIZE = 16
+CELLS_SIDE = 8
+IMG_SIZE = 128
+
+GRID_AREA = 64
+
+# TODO: define a better way to define this for different images
+# For now, I'll just focussing on solving the puzzle
+BLACK_THRESH = 40
+
+def get_pattern_vals(img):
+  start_point = CELL_SIZE / 2
+  indices_1d = [int(start_point + CELL_SIZE * x) for x in range(0, CELLS_SIDE)]
+  
+  pattern = []
+  red_count = 0
+  black_count = 0
+  
+  
+  # TODO: better sampling of color
+  for i_1d in indices_1d:
+    vals = []
+    for j_1d in indices_1d:
+      val = img[i_1d][j_1d]
+      if val > BLACK_THRESH:
+        val = 'r'
+        red_count += 1
+      else:
+        val = '0'
+        black_count += 1
+      vals.append(val)
+    pattern.append(vals)
+    
+  # ulrd = 0000
+  
+  u_edge = '1000'
+  no_edge = '0000'
+  
+  dir_nos = {
+    'd': 1,
+    'r': 10,
+    'l': 100,
+    'u': 1000
+  }
+  
+  def add_edge(edges, edge):
+    print('edges', edges, edge)
+    num = int(edges) + dir_nos[edge]
+    return "{:04d}".format(num)
+    # return str().zfill(4)
+  
+  edge_pattern = []
+  
+  
+  # Set row level prev
+  prev_edges_row = []
+  # Just
+  prev_cell_row = []
+  
+  for cell_row in pattern:
+    # Your guinea pig, row level
+    curr_edges_row = [no_edge] * 8
+    
+    if not prev_edges_row:
+      curr_edges_row = [u_edge] * 8
+      
+      
+    
+    
+    # Set cell level prev
+    prev_edge = None
+    # Just
+    cell_prev = None
+    
+    for idx, cell in enumerate(cell_row):
+      # Your guinea pig, cell level
+      curr_edge = curr_edges_row[idx]
+      
+      if not cell_prev:
+        curr_edge = add_edge(curr_edge, 'l')
+      
+      if cell_prev == cell:
+        curr_edge = add_edge(curr_edge, 'l')
+        prev_edge = add_edge(prev_edge, 'r')
+        
+      if prev_cell_row and prev_cell_row[idx] == cell:
+        # Put down in that one and up in current
+        prev_edges_row[idx] = add_edge(prev_edges_row[idx], 'd')
+        curr_edge = add_edge(curr_edge, 'u')
+      
+      
+      # Add PREV edge to row, and update it
+      if idx > 0:
+        curr_edges_row[idx-1] = prev_edge
+      prev_edge = curr_edge
+      # Just
+      cell_prev = cell
+      
+      
+    # Update the right for end of row
+    prev_edge = add_edge(prev_edge, 'r')
+    curr_edges_row[7] = prev_edge
+    
+    
+    # Add PREV row to pattern, and update it
+    if prev_edges_row:
+      edge_pattern.append(prev_edges_row)
+    prev_edges_row = curr_edges_row
+    # Just
+    prev_cell_row = cell_row
+    
+    
+    
+  # Update the down for end of pattern
+  prev_edges_row = [add_edge(edge, 'd') for edge in prev_edges_row]
+  edge_pattern.append(prev_edges_row)
+  
+  return {
+    'pattern': pattern,
+    'red_count': red_count,
+    'black_count': black_count,
+    'total_count': GRID_AREA,
+    'edge_pattern': edge_pattern
+  }
+  
+def get_holes(cell):
+  trav = []
+  untrav = []
+  
+  holes = {}
+  
+  # Do flood fills for each of the untrav, with diff color each time, and keep adding in trav
+  
+  return holes
+  
+def flood_fill(cell):
+  trav = []
+  to_trav = []
+  return trav
 
 def show_image(image_obj, caption = ''):
   cv2.imshow(caption, image_obj)
@@ -73,121 +212,107 @@ def get_intersection(line1, line2):
   
   return [x, y]
 
-img = cv2.imread('./images/97_1.jpg', 0)
-# img = cv2.imread('./images/45_2.jpg', 0)
-# img = cv2.imread('./images/61_3.jpg', 0)
-img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
 
-h, w = img.shape[:2]
-mask_color = (0, 0, 0)
+def get_pattern():
+  # img = cv2.imread('./images/97_1.jpg', 0)
+  # img = cv2.imread('./images/45_2.jpg', 0)
+  # img = cv2.imread('./images/61_2.jpg', 0)
+  # img = cv2.imread('./images/65_2.jpg', 0)
+  img = cv2.imread('./images/18_1.jpg', 0)
+  img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
 
-hull_mask = img.copy()
-hull_mask = cv2.rectangle(hull_mask, (0, 0), (w, h), mask_color, -1)
-hull_mask_lines = hull_mask.copy()
+  h, w = img.shape[:2]
+  mask_color = (0, 0, 0)
 
-# show_image(img)
-# show_image(hull_mask)
+  hull_mask = img.copy()
+  hull_mask = cv2.rectangle(hull_mask, (0, 0), (w, h), mask_color, -1)
+  hull_mask_lines = hull_mask.copy()
 
-# # imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-imcolor = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-ret, thresh = cv2.threshold(img, 100, 255, 1)
+  # show_image(img)
+  # show_image(hull_mask)
 
-
-kernel = np.ones((5,5), np.uint8)
-img_erosion = cv2.erode(thresh, kernel, iterations=1)
-img_dilation = cv2.dilate(img_erosion, kernel, iterations=2)
-img_erosion = cv2.erode(img_dilation, kernel, iterations=1)
-
-# show_image(img_erosion, 'erod')
-# show_image(img_dilation, 'dil')
-
-contours, hierarchy = cv2.findContours(img_erosion,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-img1 = cv2.drawContours(imcolor, contours, -1, (0,0,255), 3)
-
-show_image(img1, 'ctrs')
-
-areas = [cv2.contourArea(cnt) for cnt in contours]
-max_area = max(areas)
-cnt = contours[areas.index(max_area)]
-print(areas)
-# print(cnt)
-print(len(contours))
-
-hull = cv2.convexHull(cnt)
-simplified_cnt = cv2.approxPolyDP(hull,0.001*cv2.arcLength(hull,True),True)
-# hull = cv2.convexHull(simplified_cnt)
-
-img2 = cv2.drawContours(imcolor, [simplified_cnt], 0, (0,255,0), 3)
-
-hull_mask_drawn = cv2.drawContours(hull_mask, [simplified_cnt], 0, (255,255,255), 2)
-
-show_image(img2, 'simplifiedctrs')
-show_image(hull_mask_drawn)
+  # # imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+  imcolor = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+  ret, thresh = cv2.threshold(img, 100, 255, 1)
 
 
-# edges = cv2.Canny(hull_mask_drawn,50,150,apertureSize = 5)
-# show_image(edges, "edges")
-minLineLength = 100
-maxLineGap = 10
-lines = cv2.HoughLinesP(hull_mask_drawn,1,np.pi/180,100,minLineLength,maxLineGap)
-# print(lines, lines[0])
-lines_as_coeffs = []
-for line in lines:
-    x1,y1,x2,y2 = line[0]
+  kernel = np.ones((5,5), np.uint8)
+  img_erosion = cv2.erode(thresh, kernel, iterations=1)
+  img_dilation = cv2.dilate(img_erosion, kernel, iterations=2)
+  img_erosion = cv2.erode(img_dilation, kernel, iterations=1)
+
+  # show_image(img_erosion, 'erod')
+  # show_image(img_dilation, 'dil')
+
+  contours, hierarchy = cv2.findContours(img_erosion,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+  img1 = cv2.drawContours(imcolor, contours, -1, (0,0,255), 3)
+
+  # # foo 1
+  # show_image(img1, 'ctrs')
+
+  areas = [cv2.contourArea(cnt) for cnt in contours]
+  max_area = max(areas)
+  cnt = contours[areas.index(max_area)]
+  print(areas)
+  # print(cnt)
+  print(len(contours))
+
+  hull = cv2.convexHull(cnt)
+  simplified_cnt = cv2.approxPolyDP(hull,0.001*cv2.arcLength(hull,True),True)
+  # hull = cv2.convexHull(simplified_cnt)
+
+  img2 = cv2.drawContours(imcolor, [simplified_cnt], 0, (0,255,0), 3)
+
+  hull_mask_drawn = cv2.drawContours(hull_mask, [simplified_cnt], 0, (255,255,255), 2)
+
+  # # foo 2
+  # show_image(img2, 'simplifiedctrs')
+  # show_image(hull_mask_drawn)
+
+
+  # edges = cv2.Canny(hull_mask_drawn,50,150,apertureSize = 5)
+  # show_image(edges, "edges")
+  minLineLength = 100
+  maxLineGap = 10
+  lines = cv2.HoughLinesP(hull_mask_drawn,1,np.pi/180,100,minLineLength,maxLineGap)
+  # print(lines, lines[0])
+  lines_as_coeffs = []
+  for line in lines:
+      x1,y1,x2,y2 = line[0]
     
-    a, b, c = get_line_coeffs(x1,y1,x2,y2)
-    # print(line[0], a, b, c)
+      a, b, c = get_line_coeffs(x1,y1,x2,y2)
+      # print(line[0], a, b, c)
     
-    lines_as_coeffs.append((a, b, c))
+      lines_as_coeffs.append((a, b, c))
     
-    # even if you categorize, WE NEED ALL EXACT values, to get intersection points. I think.
+      # even if you categorize, WE NEED ALL EXACT values, to get intersection points. I think.
     
-    cv2.line(hull_mask_lines,(x1,y1),(x2,y2),(155,155,155),2)
+      cv2.line(hull_mask_lines,(x1,y1),(x2,y2),(155,155,155),2)
     
-show_image(hull_mask_lines, "LINES")
+  # # foo 3
+  # show_image(hull_mask_lines, "LINES")
 
-lbc = get_lines_by_categories(lines_as_coeffs, w, h)
-top_left = get_intersection(lbc['up_hori'][0], lbc['left_vert'][0])
-top_right = get_intersection(lbc['up_hori'][0], lbc['right_vert'][0])
-bottom_left = get_intersection(lbc['down_hori'][0], lbc['left_vert'][0])
-bottom_right = get_intersection(lbc['down_hori'][0], lbc['right_vert'][0])
+  lbc = get_lines_by_categories(lines_as_coeffs, w, h)
+  top_left = get_intersection(lbc['up_hori'][0], lbc['left_vert'][0])
+  top_right = get_intersection(lbc['up_hori'][0], lbc['right_vert'][0])
+  bottom_left = get_intersection(lbc['down_hori'][0], lbc['left_vert'][0])
+  bottom_right = get_intersection(lbc['down_hori'][0], lbc['right_vert'][0])
 
+  cnt = np.array([[top_left],[top_right],[bottom_left],[bottom_right]], dtype=np.single)
 
+  H,mask = cv2.findHomography(cnt, np.array([[[0., 0.]],[[128., 0.]],[[0., 128.]],[[128.,128.]]],dtype=np.single), cv2.RANSAC)
+  cropped_pattern = cv2.warpPerspective(img,H,(128, 128))
 
-
-
-# # find these as intersections of the hough lines
-# x1 = 99999
-# y1 = 99999
-# x2 = 0
-# y2 = 0
-#
-# for coord in simplified_cnt:
-#   x, y = coord[0]
-#   if x < x1:
-#     x1 = x
-#   if y < y1:
-#     y1 = y
-#   if x > x2:
-#     x2 = x
-#   if y > y2:
-#     y2 = y
-#
-# print(x1, y1, x2, y2)
-
-# TODO: REMOVE!!! This was only to prove that findHomography needs just 4 input points
-# simplified_cnt has to be further simplified to four points
-# (probably easy math)
-# Update: simplified_cnt actually works well for well-lit, higher contrast photos :P
-cnt = np.array([[top_left],[top_right],[bottom_left],[bottom_right]], dtype=np.single)
-
-H,mask = cv2.findHomography(cnt, np.array([[[0., 0.]],[[128., 0.]],[[0., 128.]],[[128.,128.]]],dtype=np.single), cv2.RANSAC)
-
-final_image = cv2.warpPerspective(img,H,(128, 128))
-
-show_image(final_image, 'cropped')
+  # # foo 4
+  # show_image(cropped_pattern, 'cropped')
 
 
+  pattern_vals = get_pattern_vals(cropped_pattern)
+
+  # for row in pattern_vals:
+#     print(" ".join(row))
+
+  return pattern_vals
 
 
 
---
+++ solver/solver.py
@@ -0,0 +1,559 @@
+# The most basic representations:
+# Either a plain 1D coord + data list
+# Or a 2D with just data, no coords (because the indices of the 2d elements IS the coords)
+
+# You HAVE to implement a binary tree
+# And you have to figure out how to know that the run failed
+
+# Start with the most crooked oedge of a shape first, the one with the most delta.
+# Straight smooth edges have very little information
+
+SHAPE_COORD_LIMIT = 3
+HORI_WINDOW_SIZE = [3,2]
+VERT_WINDOW_SIZE = [2,3]
+DEVIATION_WEIGHT = 0.15
+
+ROT_90_EDGE_MAP = {
+    'u': 'l',
+    'l': 'd',
+    'd': 'r',
+    'r': 'u'
+}
+
+OPP_EDGES_MAP = {
+    'u': 'd',
+    'd': 'u',
+    'l': 'r',
+    'r': 'l'
+}
+
+EDGE_OPNS = {
+    'u': [-1, 0],
+    'd': [1, 0],
+    'l': [0, -1],
+    'r': [0, 1],
+}
+
+    
+SHAPES = {
+    'l-left': {
+        'name': 'l-left',
+        'coords': [ [0,0], [1,0], [1,1], [1,2] ],
+        'deviated': 0
+    },
+    'l-right': {
+        'name': 'l-right',
+        'coords': [ [0,2], [1,0], [1,1], [1,2] ],
+        'deviated': 0
+    },
+    't': {
+        'name': 't',
+        'coords': [ [0,1], [1,0], [1,1], [1,2] ],
+        'deviated': 1
+    },
+    'z-left': {
+        'name': 'z-left',
+        'coords': [ [0,0], [0,1], [1,1], [1,2] ],
+        'deviated': 1
+    },
+    'z-right': {
+        'name': 'z-right',
+        'coords': [ [0,1], [0,2], [1,0], [1,1] ],
+        'deviated': 1
+    }
+}
+
+pattern1 = [ [0,0], [0,1], [0,2], [0,3], [1,0], [1,3], [2,0], [2,3] ]
+
+pattern2 = [ 
+    [0,0], [0,1], [0,2], [0,3], 
+    [1,0], [1,1], [1,2], [1,3], 
+    [2,0], [2,1], [2,2], [2,3] 
+]
+
+windowable_pattern1 = [ 
+    [0,0], [0,1], [0,2], [0,3], 
+    [1,0], [1,1], [1,2], [1,3], 
+    [2,0], [2,1], [2,2], [2,3],
+    [3,0], [3,1], [3,2],
+           [4,1]
+]
+
+windowable_pattern2 = [ 
+    [0,0], [0,1], [0,2], [0,3], 
+    [1,0], [1,1], [1,2], [1,3], 
+    [2,0], [2,1], [2,2], [2,3],
+           [3,1], [3,2],
+           [4,1], [4,2],
+]
+
+windowable_pattern3 = [ 
+    [0,0], [0,1], [0,2], 
+    [1,0], [1,1], [1,2], [1,3], 
+    [2,0], [2,1], [2,2], [2,3], [2,4], 
+]
+
+def get_shape_scores_window(window_grid, shapes=SHAPES.values()):
+    all_scores = []
+    
+    for shape in shapes:
+        name = shape['name']
+        coords = shape['coords']
+        score = is_shape_inside_window(get_coords_from_grid(window_grid), coords)
+        shape_grid = gen_obj_grid(coords)
+        
+        if score:
+            # print(name)
+#             print_pattern(coords)
+            
+            edge_score = calc_edges_score(window_grid, shape_grid)
+            
+            edge_score['shape'] = name
+            edge_score['rot'] = 0
+            edge_score['final_W'] = edge_score['W_X_S'] + DEVIATION_WEIGHT * shape['deviated']
+            
+            all_scores.append(edge_score)
+        
+        rotated_180_shape_grid = get_180_rotated(shape_grid)
+        if is_shape_inside_window(get_coords_from_grid(window_grid), get_coords_from_grid(rotated_180_shape_grid)):
+                        #
+            # print('ROT')
+            #
+            # print_pattern(get_coords_from_grid(rotated_180_shape_grid))
+        
+            edge_score = calc_edges_score(window_grid, rotated_180_shape_grid)
+            edge_score['shape'] = name
+            edge_score['rot'] = 1
+            edge_score['final_W'] = edge_score['W_X_S'] + DEVIATION_WEIGHT * shape['deviated']
+            
+            all_scores.append(edge_score)
+            
+    return all_scores
+            
+def calc_edges_score(window_grid, shape_grid):
+    shape_edges_count = 0
+    matched_edges_count = 0
+    window_edges_count = 0 
+    
+    # just to track TODO: remove
+    common_edges_list = []
+             
+    for row in window_grid:
+        for block in row:
+            if block:
+                window_edges_count += len(block['edges'])
+                if len(shape_grid[0]) >= block['coord'][1] + 1:
+                    # TODO: needs custom rules for how a square 
+                    # (or a smaller piece than window) 
+                    # should be placed
+                    # And also rotation should be factored in
+                    shape_block = shape_grid[block['coord'][0]][block['coord'][1]]
+                    if shape_block:
+                        s_edge_set = set(shape_block['edges'])
+                        w_edge_set = set(block['edges'])
+            
+                        shape_edges_count += len(shape_block['edges'])
+                        common_edges = list(w_edge_set.intersection(s_edge_set))
+                        
+                        # just to track TODO: remove
+                        common_edges_list.append(common_edges) 
+                        
+                        matched_edges_count += len(common_edges)
+                        
+              
+    w_match = matched_edges_count/window_edges_count 
+    s_match = matched_edges_count/shape_edges_count
+    w_x_s = w_match * s_match 
+      
+    return {
+        # 'SE': shape_edges_count,
+        # 'WE': window_edges_count,
+        # 'ME': matched_edges_count,
+        #
+        # 'W_MATCH': w_match,
+        # 'S_MATCH': s_match,
+        'W_X_S': w_x_s,
+        
+        'common_edges': common_edges_list
+    }
+
+
+def is_shape_inside_window(window, shape):
+    window_set = set([str(p[0]) + str(p[1]) for p in window])
+    shape_set = set([str(p[0]) + str(p[1]) for p in shape])
+
+    is_present = shape_set.issubset(window_set)
+    
+    if is_present:
+        return {
+            'boundary_matches': [5,10],
+            'edge_touching_blocks': [3, 4],
+            'is_deviated_shape': 0
+        }
+        
+def get_coords_from_grid(grid):
+    coords = []
+    for row in grid:
+        for block in row:
+            if block:
+                coords.append(block['coord'])
+    return coords
+    
+    
+# TODO: this repeats obj_grid's work
+# abstract out taking care of null blocks
+def print_pattern(points):
+    height = max([p[0] for p in points]) + 1
+    length = max([p[1] for p in points]) + 1
+    coords = points
+    for i in range(height):
+        row_points = [p for p in coords if p[0] == i]
+        s = ''
+        for j in range(length):
+            if [i, j] in row_points:
+                s += ' ▢'
+            else:
+                s += '  '
+        print(s)  
+        
+# def print_pattern_with_edges():
+#     height =
+    
+    
+def gen_obj_grid(points):
+    obj_grid = []
+    prev_blocks = []
+    prev_coords = []
+    
+    height = max([p[0] for p in points]) + 1
+    length = max([p[1] for p in points]) + 1
+    
+    for i in range(height):
+        blocks = [] 
+        all_blocks = []
+        
+        row_edge = ''
+        if i == 0:
+            row_edge = 'u'
+        elif i == height - 1:
+            row_edge = 'd'
+            
+        row_coords = [p for p in points if p[0] == i]
+        
+        if prev_blocks:
+            for block in prev_blocks:
+                down_coord = block['coord'][:]
+                down_coord[0] = i
+                if down_coord not in row_coords:
+                    block['edges'].append('d')
+        
+        coords_parsed = 0
+        prev = None
+        for j in range(length):
+            coord = [i, j]
+            if coord in row_coords:
+                
+                edges = []
+                if row_edge:
+                    edges.append(row_edge)
+                    
+                if prev_coords:
+                    up_coord = coord[:]
+                    up_coord[0] -= 1
+                    if up_coord not in prev_coords:
+                        edges.append('u')
+                    
+                if not prev:
+                    edges.append('l')
+                
+                coords_parsed += 1
+                if coords_parsed == len(row_coords):
+                    edges.append('r') 
+                
+                block = {
+                    'coord': coord,
+                    'edges': edges
+                }
+                
+                all_blocks.append(block)
+                blocks.append(block)
+                
+                prev = coord
+            else:
+                if blocks:
+                    last_edges = blocks[-1]['edges']
+                    if 'r' not in last_edges:
+                        last_edges.append('r')
+                    prev = None
+                    
+                # TODO: use all blocks for real to check up and down everything
+                # Your losing much information in the current implementation adn running 
+                # too many checks for up and down edge checks
+                
+                all_blocks.append(None)
+                
+        obj_grid.append(all_blocks)
+        prev_blocks = blocks
+        prev_coords = row_coords
+        prev_coords = row_coords
+        
+    return obj_grid
+    
+    
+def get_window(patt, coord, size):
+    l, h = size
+    y0, x0 = coord
+    
+    import copy
+
+    def offset_coords(row):
+        new_row = []
+        for block in row:
+            b = None
+            if block:
+                b = copy.copy(block)
+                c = b['coord']
+                b['coord'] = [c[0] - y0, c[1] - x0]
+            new_row.append(b)
+        return new_row
+        
+    window_grid = [offset_coords(patt[row_no][x0:x0+l]) for row_no in range(y0, y0+h)]
+    return {
+        'grid': window_grid,
+        'coord': coord
+    }
+    
+    
+def get_90_rotated(patt):
+    l = len(patt[0])
+    h = len(patt)
+    
+    import copy
+    
+    rotated_patt = []
+    
+    ir = 0
+    for i in range(l-1, -1, -1):  
+      row = []
+      jr = 0
+      
+      for j in range(h):
+        b = copy.copy(patt[j][i])
+        if b:
+            b['coord'] = [ir, jr]
+            b['edges'] = [ROT_90_EDGE_MAP[e] for e in b['edges']]
+        row.append(b)
+        jr += 1
+        
+      rotated_patt.append(row)
+      ir += 1
+      
+    return rotated_patt
+    
+    
+def get_180_rotated(patt):
+    l = len(patt[0])
+    h = len(patt)
+    
+    import copy
+    
+    rotated_patt = []
+    
+    ir = 0
+    for i in range(h - 1, -1, -1):
+        row = []
+        jr = 0
+        
+        for j in range(l - 1, -1, -1):
+            b = copy.copy(patt[i][j])
+            if b:
+                b['coord'] = [ir, jr]
+                b['edges'] = [OPP_EDGES_MAP[e] for e in b['edges']]
+            row.append(b)
+            jr += 1
+
+        rotated_patt.append(row)
+        ir += 1
+
+    return rotated_patt
+    
+
+def get_x_direction_windows(patt):
+    l = len(patt[0])
+    h = len(patt)
+    
+    # [0, 0], [0, 1]
+    # [3, 0], [3, 1]
+    w_size = HORI_WINDOW_SIZE 
+  
+    wl, wh = w_size
+  
+    c_range = range(l - wl + 1) 
+    offset_c = h - wh
+  
+    start_coords = [[0, c] for c in c_range]
+    end_coords = [[offset_c, c] for c in c_range]
+    
+    # TODO: shift window down if >= half empty
+  
+    windows = [get_window(patt, c, w_size) for c in start_coords + end_coords]
+
+    return windows
+
+
+def get_all_options(patt, shapes):
+    options = []
+    patt_windows = get_x_direction_windows(patt)
+    
+    for w in patt_windows:
+        win = w['grid']
+        
+        scores = get_shape_scores_window(win, shapes)
+        
+        if scores:
+            option = sorted(scores, key=lambda x: x['final_W'], reverse=True)[0]
+            option['coord'] = w['coord']
+            options.append(option)
+    
+    return options
+    
+    
+def solve_patt_with_pieces(patt_values, shapes_set=None):
+    import copy
+    
+    solution = []
+    
+    shapes = copy.deepcopy(SHAPES)
+    if shapes_set:
+        shapes = {}
+        for name in SHAPES:
+            if name in shapes_set:
+                shapes[name] = SHAPES[name]
+
+    patt = gen_obj_grid(patt_values)
+    
+    while patt:
+        print("NOW PATT")
+        print_pattern(get_coords_from_grid(patt))
+        # print(patt)
+        
+        rotated_patt = get_90_rotated(patt)
+        print_pattern(get_coords_from_grid(rotated_patt))
+    
+        patt_options = get_all_options(patt, shapes.values())
+        rot_patt_options = get_all_options(rotated_patt, shapes.values())
+    
+        patt_opt = get_best_option(patt_options) if patt_options else None
+        print('rot_options')
+        rot_patt_opt = get_best_option(rot_patt_options) if rot_patt_options else None
+    
+        def fill(pattern, option):
+            patt_clone = copy.copy(pattern)
+            solution.append(option)
+            shape = shapes.pop(option['shape'])
+            return fill_piece(patt_clone, option['coord'], shape, option['rot'])
+        
+        if patt_opt and rot_patt_opt:
+            if patt_opt['final_W'] > rot_patt_opt['final_W']:
+                patt = fill(patt, patt_opt)
+            else:
+                patt = fill(rotated_patt, rot_patt_opt)
+            
+        elif patt_opt:
+            patt = fill(patt, patt_opt)
+        elif rot_patt_opt:
+            patt = fill(rotated_patt, rot_patt_opt)
+        else:
+            # TODO: Something weird happened. Analyse and rerun.
+            print('Something weird happened. Analyse and rerun.')
+            print_pattern(get_coords_from_grid(patt))
+            break
+
+        patt = get_trimmed_pattern(patt)
+        
+    
+    # print('final_patt', patt, shapes)
+    for piece in solution:
+        print(piece['coord'])
+        print(piece['shape'])
+        print(piece['rot'])
+        print(piece['final_W'])
+        print('------------------')
+    # print(solution)
+    
+    
+def get_best_option(options):
+    options = sorted(options, key=lambda x: x['final_W'], reverse=True)
+    for o in options:
+        print(o['shape'], o['final_W'], o['rot'])
+    return options[0]
+    
+    
+def fill_piece(patt, coord, shape, rotate=False):
+    shape_grid = gen_obj_grid(shape['coords'])
+    if rotate:
+        shape_grid = get_180_rotated(shape_grid)
+        
+    for row in shape_grid:
+        for block in row:
+            
+            if block:
+                curr_coord = [block['coord'][0] + coord[0], block['coord'][1] + coord[1]]
+                patt[curr_coord[0]][curr_coord[1]] = None
+                edges = block['edges'] or []
+                
+                transfer_edges(patt, edges, curr_coord)
+
+    return patt
+    
+
+def transfer_edges(patt, edges, curr_coord):
+    """
+        MUTATEs Pattern
+    """
+    l = len(patt[0])
+    h = len(patt)
+    for e in edges:
+        opn = EDGE_OPNS[e]
+        y = curr_coord[0] + opn[0]
+        x = curr_coord[1] + opn[1]
+        
+        if y >=0 and x >= 0 and y < h and x < l:
+            neighbour = patt[y][x]
+            if neighbour:
+                opp_edge = OPP_EDGES_MAP[e]
+                if neighbour['edges']:
+                    neighbour['edges'].append(opp_edge)
+                else:
+                    neighbour['edges'] = [opp_edge]
+                    
+                    
+def get_trimmed_pattern(patt):
+    current_patt = []
+    
+    for row in patt:
+        if not all(x == None for x in row):
+            current_patt.append(row)
+    
+    if current_patt:
+        rotated = get_180_rotated(current_patt)
+    
+        current_patt = []
+        for row in rotated:
+            if not all(x == None for x in row):
+                current_patt.append(row)
+            
+    return current_patt
+
+
+
+
+def solve():
+    # Not working. Test and when it does, build a tree model to backtrack to what it did. (Warning: tricky and involves recursion and trees. Do a simple ds in isolation first to see if you do get it.)
+
+    # solve_patt_with_pieces(windowable_pattern3, ['l-left', 't', 'z-left'])
+    solve_patt_with_pieces(windowable_pattern3, ['l-right', 't', 'z-right'])
+
+
+if __name__ == '__main__':
+    solve()
